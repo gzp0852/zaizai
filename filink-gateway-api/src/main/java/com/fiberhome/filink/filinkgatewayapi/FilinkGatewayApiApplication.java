0d0a5adbf0db6d0450b6a3c63ec7b39b8de52a99
gzp0852 <36258139+gzp0852@users.noreply.github.com>
2019-04-27 21:56:49 +0800

    Add files via upload
    api

---
+++ filink-gateway-api/src/main/java/com/fiberhome/filink/filinkgatewayapi/FilinkGatewayApiApplication.java
@@ -0,0 +1,14 @@
+package com.fiberhome.filink.filinkgatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilinkGatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilinkGatewayApiApplication.class, args);
+    }
+
+}
+
